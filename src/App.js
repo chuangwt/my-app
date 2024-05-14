55dba396de646137b5dc0d4e84b2ff6644fb1545
chuangwt <113202785+chuangwt@users.noreply.github.com>
2024-05-14 23:37:16 +0800

    create

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import MySlider from './MySlider';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello React! 2024.5.2</h1>
+        <h2>React第一個元件</h2>
+        <MySlider color="RED" />
+        <MySlider color="GREEN"/>
+        <MySlider color="BLUE"/>
       </header>
     </div>
   );
