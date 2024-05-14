674121ea9fbfbe4bf1e21420b37a9f31402c602d
chuangwt <113202785+chuangwt@users.noreply.github.com>
2024-05-15 01:33:04 +0800

    新增MyRGBPanel元件

--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
-function MySlider(props) {
+function MySlider(label, onChange) {
    const [value,setValue]=useState(0);
-   const handleChange =(e)=>{
+   const handleSilderChange =(e)=>{
     setValue(e.target.value)
+    if(onChange){
+        onChange(e.target.value)
+    }
    };
     return (
         <div>
-            <span>{props.color}</span>
+            <span>{label}</span>
             <input 
             type="range" 
             width="200" 
             min="0" 
             max="255" 
             value={value} 
-            onChange={handleChange}/>
+            onChange={handleSilderChange}/>
             <span>{value}</span>
         </div>
            
