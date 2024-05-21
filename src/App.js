65e8ea5584a450d7fdd79daa288337939eb30f2d
chuangwt <113202785+chuangwt@users.noreply.github.com>
2024-05-21 19:33:37 +0800

    create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import MyRGBPanel from './components/MyRGBPanel';
 import MyCalculator from './components/MyCalculator';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MyRGBBPanel</h1>
+        <h1>Hello React! 2024.5.16</h1>
+        <h2>MyRGBPanel</h2>
         <MyRGBPanel />
-        <h2>My Calculator</h2>
+        <h2>MyCalculator</h2>
         <MyCalculator />
       </header>
     </div>
