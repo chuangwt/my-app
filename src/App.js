f348cb58bb6ef438bd028cb138fcac1995be26af
chuangwt <113202785+chuangwt@users.noreply.github.com>
2024-06-02 23:05:05 +0800

    create

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
-import MyRGBPanel from './components/MyRGBPanel';
-import MyCalculator from './components/MyCalculator';
+import "./App.css";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello React! 2024.5.16</h1>
-        <h2>4B0G0178 莊惟婷</h2>
-        <h2>MyRGBPanel</h2>
-        <MyRGBPanel />
-        <h2>MyCalculator</h2>
-        <MyCalculator />
-      </header>
+      <h1>4B0G0178 莊惟婷</h1>
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
     </div>
   );
 }
